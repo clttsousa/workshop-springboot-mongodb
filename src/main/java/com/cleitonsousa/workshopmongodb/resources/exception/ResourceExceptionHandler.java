74733e536de03c03709a7c5427737a8145fa0435
Cleiton Sousa <clttsousa@hotmail.com>
2023-11-13 14:27:05 -0300

    Obtendo usuario por id

---
+++ src/main/java/com/cleitonsousa/workshopmongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.cleitonsousa.workshopmongodb.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.cleitonsousa.workshopmongodb.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> 
+	objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis()
+				, status.value(), "Não encontrado", e.getMessage()
+				, request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
