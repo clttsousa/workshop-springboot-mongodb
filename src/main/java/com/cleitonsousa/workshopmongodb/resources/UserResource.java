d73e2b78fb478a0b3b3e3387d93b32be3bc7321f
Cleiton Sousa <clttsousa@hotmail.com>
2023-11-14 14:53:23 -0300

    Endpoint para retornar posts de um usuario

--- src/main/java/com/cleitonsousa/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/cleitonsousa/workshopmongodb/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.cleitonsousa.workshopmongodb.domain.Post;
 import com.cleitonsousa.workshopmongodb.domain.User;
 import com.cleitonsousa.workshopmongodb.dto.UserDTO;
 import com.cleitonsousa.workshopmongodb.services.UserService;
@@ -63,5 +64,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
