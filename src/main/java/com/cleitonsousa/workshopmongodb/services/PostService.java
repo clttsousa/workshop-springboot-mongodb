7f21de7d9a8ec3da5ca0d6a8c6bb82d5ecb8444f
Cleiton Sousa <clttsousa@hotmail.com>
2023-11-14 14:57:30 -0300

    Obter Post por id

---
+++ src/main/java/com/cleitonsousa/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cleitonsousa.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cleitonsousa.workshopmongodb.domain.Post;
+import com.cleitonsousa.workshopmongodb.repository.PostRepository;
+import com.cleitonsousa.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional <Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
