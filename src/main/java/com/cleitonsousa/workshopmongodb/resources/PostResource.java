fa344d252255b7e8918f3ae10518b2dfd5b735a4
Cleiton Sousa <clttsousa@hotmail.com>
2023-11-14 15:25:13 -0300

    Consulta com query methods

--- src/main/java/com/cleitonsousa/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/cleitonsousa/workshopmongodb/resources/PostResource.java
@@ -1,14 +1,18 @@
 package com.cleitonsousa.workshopmongodb.resources;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cleitonsousa.workshopmongodb.domain.Post;
+import com.cleitonsousa.workshopmongodb.resources.util.URL;
 import com.cleitonsousa.workshopmongodb.services.PostService;
 
 @RestController
@@ -18,6 +22,13 @@ public class PostResource {
 	@Autowired
 	private PostService service;
 	
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List <Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id){
